6bd9228c72f1e4685a003fc7a64805f694db0551
ryucm <ckd12394@gmail.com>
2022-09-06 16:27:15 +0900

    Add projectapp

--- accountapp/views.py
+++ accountapp/views.py
@@ -7,6 +7,9 @@ from accountapp.models import HelloWorld
 from django.contrib.auth.models import User
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.list import MultipleObjectMixin
+
+from articleapp.models import Article
 from .forms import AccountUpdateForm
 from .decorators import account_ownership_required
 # Create your views here.
@@ -35,10 +38,16 @@ class AccountCreateView(CreateView):
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/create.html'
 
-class AccountDetailView(DetailView):
+class AccountDetailView(DetailView, MultipleObjectMixin):
     model = User
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
+    
+    paginate_by = 25
+    
+    def get_context_data(self, **kwargs):
+        object_list = Article.objects.filter(writer=self.get_object())
+        return super(AccountDetailView, self).get_context_data(object_list=object_list, **kwargs)
 
 @method_decorator(has_ownership, 'get')
 @method_decorator(has_ownership, 'post')
---
+++ projectapp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from .views import ProjectListView, ProjectCreateView, ProjectDetailView
+
+app_name = 'projectapp'
+
+urlpatterns = [
+    path('create/', ProjectCreateView.as_view(), name='create'),
+    path('list/', ProjectListView.as_view(), name='list'),
+    path('detail/<int:pk>', ProjectDetailView.as_view(), name='detail'),
+]
